e3a3d20190049fce7d262956420d3ef54dbe2501
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-08-12 16:15:19 -0400

    made index.js display an array of 150 pokemon

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }//random num generator
+
 
 const http = require('http')//http object
 let nerds = require('nerds');
-let multiPoke = nerds.resolve('Pokemon', 3).include(['name', 'type', 'hp']).asArray();
+let multiPoke = nerds.resolve('Pokemon', 150).include(['name', 'type', 'hp']).asArray();
 
 // console.log(multiPoke[0].name);
 
 
 const server = http.createServer((req, res)=>{//creates the server
-    const randNum= Math.floor(Math.random() * 3);
+    const randNum= Math.floor(Math.random() * 150);
     res.end(`<h1>${multiPoke[randNum].name}</h1>`);
 });
 
